3687ba155c8df2eb97f9f39245dccb2786a9e62e
kuiil7 <jgamerino@gmail.com>
2024-03-07 12:00:42 -0800

    updated landingpage

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,24 +16,23 @@ Your browser does not support the video tag.
     <div class=" has-text-centered">
     <div className=" " >
 <div className="overlay  "></div>
-        <p  className="subtitle is-6 slide-text ml-2 has-text-centered has-text-white   " > It is time to get out of the dark and</p>
-        <p  className="title is-1 appear-text has-text-centered has-text-white   " >Fill </p>
-        <p  className="title is-1 appear-text has-text-centered has-text-white  " > The </p>
-        <p  className="title is-1 appear-text has-text-centered has-text-white  " > Gap</p>
-
-
-        <div className="has-text-centered  ">       
-          <i class="fa-solid  mr-5 fa-4x fa-brain  appear-text4 has-text-danger"></i>
-        <i class="fa-solid  mr-5 fa-4x fa-ear-deaf appear-text5 has-text-info"></i>
-        <i class="fa-solid  mr-5 fa-4x fa-wheelchair-move appear-text6 has-text-success"></i>
-        <i class="fa-solid  mr-5 fa-4x fa-eye appear-text7 has-text-warning"></i>
+    
+<div className="has-text-centered  ">       
+          <i class="fa-solid  mr-5 fa-2x fa-brain  appear-text4 has-text-danger"></i>
+        <i class="fa-solid  mr-5 fa-2x fa-ear-deaf appear-text5 has-text-info"></i>
+        <i class="fa-solid  mr-5 fa-2x fa-wheelchair-move appear-text6 has-text-success"></i>
+        <i class="fa-solid   fa-2x fa-eye appear-text7 has-text-warning"></i>
         </div>
-
-        <p  className="subtitle is-6 slide-text2 has-text-centered has-text-white  has-text-white mb-3 mt-3  "> in the disability space.</p>
-        <p  className="is-italic appear-text mb-5 has-text-centered has-text-white"> -jgamworks.</p>
+       
+        <p  className="subtitle is-6 slide-text ml-2 mb-1 mt-2 has-text-centered has-text-white  " > It is time to get out of the dark and fill the gap within the disability space.</p>
+        
+        <p  className="is-italic appear-text   has-text-centered has-text-white"> -jm.</p>
        
         <div className="has-text-centered  ">
        <div class="tabs is-centered ">
+
+
+        
   <ul >
 
 
