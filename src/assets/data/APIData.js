2c80b738a459b021e4cd607c8131fb174f50eddc
kuiil7 <jgamerino@gmail.com>
2024-05-19 21:54:21 -0700

    updated API projects page

--- src/assets/data/APIData.js
+++ src/assets/data/APIData.js
@@ -10,7 +10,7 @@
       demo:"https://my-weather-one.vercel.app/",
       description:"User can find local temperature via browser's geolocation function and, also, do query search across U.S. cities.",
       myDuty:"Sole maintainer.",
-      builtWith:"Bulma, Axios, Moments, Geolocation, and dotenv.",
+      builtWith:"Bulma, Axios, Moments, Geolocation, and https://openweathermap.org/",
       url:"https://openweathermap.org/",
       status: "Completed",
       notes:"Considering adding extra features in v2."
@@ -24,27 +24,30 @@
           demo:"https://mymovies-amber.vercel.app/",
           description: "A website where user can search and find movies, TV shows, and actors along with trending, popularity, and other categories",
           myDuty: "Sole maintainer.",
-          builtWith: "Luxon, Axios, dotenv, Material-UI, and Bulma.",
+          builtWith: "Luxon, Axios, dotenv, and Bulma and https://www.themoviedb.org/",
           url:"https://www.themoviedb.org/",
           status: "Completed",
-          notes:"Pagination to be added in v2"
+          notes:""
 
 
         },
         {
           id: 2,
-          title: "MyNews",
-          images:( process.env.PUBLIC_URL +  "/assets/images/repos/mynews.png"),
+          title: "Where's My Beer?",
+          images:( process.env.PUBLIC_URL +  "/assets/images/repos/wmb.png"),
+          repo:"https://github.com/Kuiil7/wmb",
           alt: "screenshot of app",
-          demo:null,
-          description: "Search and find U.S. news sources. Includes Top Headlines Health, Entertainment, Sports, Science, and Technology on load.",
+          description: "Search and find microbreweries across the United States",
           myDuty:"Sole maintainer",
-          builtWith: "Bulma, FontAwesome, Luxon, dotenv, and Express.",
-          url:"https://newsapi.org/",
-          status: "Ongoing",
-          notes: "APP is functional in development. Needs serverless function for production."
+          builtWith: "Bulma, FontAwesome, Luxon, dotenv, and https://www.openbrewerydb.org/",
+          demo: "https://wonderful-mccarthy-55ff72.netlify.app/",
+          url:"https://www.openbrewerydb.org/",
+          status: null,
+          notes: null
           },
 
+
+    
 
 
   ];
