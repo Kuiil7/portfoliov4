a605757b59ab3a2c5d8257c7df81bf3eb4e434e8
kuiil7 <jgamerino@gmail.com>
2024-08-13 16:51:41 -0700

    updated about me links

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,19 +13,28 @@ const AboutMe = () => {
           <p className=" is-italic ">
           I am passionate about technical creativity and innovation, particularly in the accessibility and inclusion space. With a Master's degree in Linguistics, a full-stack development certification, and extensive experience in assistive technology, I have worked with various organizations to advance digital accessibility and inclusion. My background includes roles as a Senior Assistive Technology Engineering Analyst, Project and Operations Administrator, and Outreach Consultant, where I supported accessibility standards, managed assistive technology initiatives, and engaged with diverse stakeholders. I am dedicated to bridging gaps in the disability space by leveraging my technical skills and expertise in accessibility, aiming to create a more open and inclusive digital environment for everyone.   
            </p> 
-          <p className="is-size-3 mt-3" >
-              <Link className="ml-1 navbar-item3" href="mailto:jgamerino@gmail.com">
-                <i className="fa-solid fa-envelope fa-1x mr-1"></i>
-              </Link>
-              <Link href="https://github.com/kuiil7" className=" navbar-item3">
-                <i className="fab fa-github fa-1x mr-1"></i>
-              </Link>
-              <Link href="https://www.linkedin.com/in/joe-merino-8298b6193/" className=" navbar-item3">
-                <i className="fab fa-linkedin fa-1x"></i>
-              </Link>
-            </p>
-
-         
+           <div className="container">
+      <div className="columns is-centered">
+        <div className="column is-one-third has-text-centered">
+          <Link className="navbar-item3 navbar-item" to="mailto:jgamerino@gmail.com">
+            <i className="fa-solid fa-envelope fa-2x"></i>
+            <p className="m-2">Email</p>
+          </Link>
+        </div>
+        <div className="column is-one-third has-text-centered">
+          <Link className="navbar-item3 navbar-item" to="https://github.com/kuiil7">
+            <i className="fab fa-github fa-2x"></i>
+            <p className="m-2">GitHub</p>
+          </Link>
+        </div>
+        <div className="column is-one-third has-text-centered">
+          <Link className="navbar-item3 navbar-item" to="https://www.linkedin.com/in/joe-merino-8298b6193/">
+            <i className="fab fa-linkedin fa-2x"></i>
+            <p className="m-2">LinkedIn</p>
+          </Link>
+        </div>
+      </div>
+    </div>
           </div>
         </div>
         
